31728a8db337ca1b652acf91bedab176d846d5d8
jaler <jalerking@ya.ru>
2026-04-08 13:06:57 +0300

    1

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean2 == bean1);
+        System.out.println(cat1 == cat2);
+
+        System.out.println(bean1.equals(bean2));
+        System.out.println(bean1.equals(cat2));
+
+    }
+}
